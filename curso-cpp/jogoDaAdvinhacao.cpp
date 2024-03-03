50fa99c7c7ed04ebd3e213a8cc7b8a2259f998e4
Hellen Moura <127620071+hellen-moura@users.noreply.github.com>
2024-03-03 20:46:56 -0300

    Add files via upload

---
+++ curso-cpp/jogoDaAdvinhacao.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "***************************************" << endl;
+    cout << "* Boas vindas ao Jogo da Adivinhação! *" << endl;
+    cout << "***************************************" << endl;
+
+    int numero_secreto = 42;
+    cout << "O número secreto é " << numero_secreto << ". Não conte para ninguém!" << endl;
+}
